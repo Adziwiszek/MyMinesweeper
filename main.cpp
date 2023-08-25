49e7f07789a08c36411b60f1705828728e299c9c
Adam Dziwiszek <adziwiszek1@gmail.com>
2023-08-25 15:55:20 +0200

    added basic things for creating tile map with info about bombs etc

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
+
+class TileMap : public sf::Drawable, sf::Transformable
+{
+private:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // apply the transform
+        states.transform *= getTransform();
+
+        // apply the tileset texture
+        states.texture = &m_tileset;
+
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Minesweeper");
+    sf::RenderWindow window(sf::VideoMode(640, 740), "Minesweeper");
+
+    const int level_size = 10;
+    int level_info[level_size][level_size] = {};
+    int num_of_bombs = ceil(level_size * level_size * 0.2);
 
     while (window.isOpen())
     {
